1f3ffe24ae03379ad28f9a2c975b037af7938157
sjcco <73748496+sjcco@users.noreply.github.com>
2021-04-08 12:07:47 -0500

    Fix linter error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,7 +36,8 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
